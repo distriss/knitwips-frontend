8e51eeea4c82a3fce0497876291ea8c8b91ec4dd
Triss Marsh <marshtriss@gmail.com>
2024-02-28 10:57:24 +0000

    added: token validation

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useState, useEffect } from 'react';
 import AuthContextProps from '../interfaces/AuthContextProps';
 import UserData from '../interfaces/UserData';
+import axios from 'axios';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
@@ -23,16 +24,32 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     const [authUser, setAuthUser] = useState<UserData | null>(null);
 
     useEffect(() => {
-        const storedToken = cookies.get('TOKEN')
-        const storedUser = localStorage.getItem('userInfo');
-        if (storedToken) {
-            setAuthenticated(true);
-        }
-        if (storedUser) {
-            const user: UserData = JSON.parse(storedUser);
-            setAuthUser(user);
-            setAuthenticated(true);        
-        }
+        const validateToken = async () => {
+            const storedToken = cookies.get('TOKEN')
+            const storedUser = localStorage.getItem('userInfo');
+            if (storedToken) {
+                try {
+                    await axios.get('http://localhost:5000/token/validate', {
+                        headers: { Authorization: `Bearer ${storedToken}` }
+                    });
+                    if (storedUser) {
+                        const user: UserData = JSON.parse(storedUser);
+                        setAuthUser(user);
+                        setAuthenticated(true);
+                    }
+                } catch (error) {
+                    cookies.remove('TOKEN');
+                    localStorage.removeItem('userInfo');
+                    setAuthenticated(false);
+                    setAuthUser(null);
+                }
+            } else {
+                localStorage.removeItem('userInfo');
+                setAuthenticated(false);
+                setAuthUser(null);
+            }
+        };
+        validateToken();
     }, []);
 
     return (
@@ -43,7 +60,7 @@ export default function AuthProvider({ children }: AuthProviderProps) {
                 authUser,
                 setAuthUser,
             }}
-        >
+            >
             {children}
         </AuthContext.Provider>
     )
