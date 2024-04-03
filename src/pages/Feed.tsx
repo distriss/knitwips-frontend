b869ffb450ec7d12f4d9df514ed14831a0e573fd
Triss Marsh <marshtriss@gmail.com>
2024-04-03 23:49:52 +0100

    feed bg, pattern page layout reconfig

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -42,9 +42,14 @@ export default function Feed() {
         <header>
             <NavBar />
         </header>        
-        <section className="flex flex-col justify-center items-center bg-main2 ">
-            <h1 className="mt-2">This is Feed</h1>
-            <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+        <section className="flex flex-col justify-center items-center md:px-40 py-10 px-10 bg-white drop-shadow-md">
+                <h1 className="altfont text-6xl text-accent1 md:mt-16 md:b-4 my-2">
+                    Patterns & WIPs from the community
+                </h1>
+                <span className="text-primary1 text-3xl md:mt-2 mt-6 5 md:mb-8 mb-6">
+                    Discover patterns and notes created by other users
+                </span>
+                <div className="flex items-center justify-center flex-wrap">
                 <button 
                     type="button"
                     onClick={() => { setFilter('latest'); setPage(1); setPatterns([]); }}
@@ -57,7 +62,9 @@ export default function Feed() {
                     className={`text-accent1 text-xl border-accent1 ${filter === 'likes' ? 'bg-accent1 border-accent1 text-white font-bold' : 'bg-white'} hover:bg-white hover:text-accent2 hover:border-accent2 border focus:border-accent1 focus:ring-2 focus:outline-none focus:ring-accent1 focus:scale-110 min-w-32 py-2 px-2  rounded-full text-center me-3 m-3 transition ease-in-out hover:scale-110 duration-300`}>
                         Most Liked
                 </button>
-            </div>
+            </div> 
+        </section> 
+        <section id="feed" className="flex flex-col justify-center items-center bg-main1 md:px-40 md:py-20 px-10 py-10 ">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">                
                 {patterns.map(pattern => 
                     <PatternCard key={pattern._id} pattern={pattern} />
--- src/pages/Pattern.tsx
+++ src/pages/Pattern.tsx
@@ -25,30 +25,32 @@ return (
     <header>
         <NavBar />
     </header>
-    <main className="mx-w-7xl mx-auto p-4 sm:p-6 lg:p-8 pt-6 grid grid-cols-1 md:grid-cols-3 gap-4 bg-main2 min-h-screen">
-            <div className="bg-white p-4 m-2 rounded-lg shadow">
-                <h3 className='text-2xl m-2'>wip details</h3>
-                { patternData && (
-                <ul>
-                     <li>Yarn Weight: {patternData.yarnWeight}</li>
-                     <li>Needle Size: {patternData.needleSize}</li>
-                     <li>stuff: and stuff</li>
-                     <li>stuff: and stuff</li>
-                     <li>stuff: and stuff</li>
-                </ul>
-                )}
-            </div>
-        <div className="md:col-span-2 space-y-4">
+    <main className="mx-w-7xl mx-auto p-4 sm:p-6 lg:p-8 pt-6 grid grid-cols-1 md:grid-cols-5 gap-4 bg-main2">
+        <div className="bg-white p-4 m-2 rounded-lg shadow min-h-80">
+            <h3 className='text-2xl m-2'>wip details</h3>
+            { patternData && (
+            <ul>
+                 <li>Yarn Weight: {patternData.yarnWeight}</li>
+                 <li>Needle Size: {patternData.needleSize}</li>
+                 <li>stuff: and stuff</li>
+                 <li>stuff: and stuff</li>
+            </ul>
+            )}
+        </div>
+        <div className="md:col-span-3 space-y-4 ">            
+                
+            <div className="bg-white p-4 m-2 rounded-lg shadow min-h-80">
             {patternData && <h1>{patternData.title}</h1>}
-
-            <div className="bg-white p-4 m-2 rounded-lg shadow">
-
+            <p>{patternData?.description}</p>
+                
             </div>
-        </div>          
+        </div>
+        <div className="bg-white p-4 m-2 rounded-lg shadow">
+            <span>User Info</span>
+        </div>         
     </main>
-  <section className="mx-w-7xl mx-auto p-4 sm:p-6 lg:p-8 pt-6 grid grid-cols-1 gap-4 bg-main2 min-h-screen">
-     
-  <div  className="bg-white p-4 my-6 mx- rounded-lg shadow">
+  <section  className="grid grid-cols-1 md:grid-cols-5 gap-4 mx-w-7xl mx-auto pt-6 p-4 sm:p-6 lg:p-8 bg-main2">     
+  <div  className="md:col-start-2 md:col-span-3 bg-white p-4 my-6 rounded-lg shadow min-h-80">
      <h3>comments</h3>
   </div>
   </section>
